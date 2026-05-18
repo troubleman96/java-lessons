d46d84d0510b41c0152d02ec93311a22a6fd61b6
Lugenge Emmanuel <itslugenge96@gmail.com>
2026-05-19 00:26:06 +0300

    math done

---
+++ lesson04/Main.java
@@ -0,0 +1,22 @@
+package lesson04;
+
+public class Main {
+    public static void main(String[] args){
+        //arithemtic like other langauges
+        int x = 10;
+        int y = 3;
+
+        System.out.println(x + y); // 13
+        System.out.println(x - y); // 7
+        System.out.println(x * y); // 30
+        System.out.println(x / y); // 3
+        System.out.println(x % y); // 1
+
+        int z = 5;
+        ++z;
+        System.out.println(z); // 6
+        --z;
+        System.out.println(z); // 5
+
+    }
+}

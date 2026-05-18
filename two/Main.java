f6305eca0480f643c74fe8cea62d5ab121276d5b
Lugenge Emmanuel <itslugenge96@gmail.com>
2026-05-18 22:57:50 +0300

    Add Java basics lesson

---
+++ two/Main.java
@@ -0,0 +1,23 @@
+package two;
+
+public class Main {
+    public static void main(String[] args){
+        //comments in java
+        String name = "CamelTech";
+        System.out.println(name);
+
+        //variables in java 
+        int age = 122;
+        System.out.println(age);
+
+        //basixcs haha
+        String firstName = "John ";
+        String lastName = "Doe";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+
+        int x = 5;
+        int y = 6;
+        System.out.println(x + y);
+    }
+}

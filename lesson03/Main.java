bb6ba2459c50011ed06096a8e4e45f8fea7a4c99
Lugenge Emmanuel <itslugenge96@gmail.com>
2026-05-19 00:17:37 +0300

    lessons on typecasting

--- lesson03/Main.java
+++ lesson03/Main.java
@@ -2,6 +2,10 @@ package lesson03;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Lesson 3 placeholder");
+        //type casting in java
+        int myInt = 9;
+        double myDouble = myInt; // automatic casting: int to double
+        System.out.println(myInt);      // Outputs 9
+        System.out.println(myDouble);   // Outputs 9.0
     }
 }
